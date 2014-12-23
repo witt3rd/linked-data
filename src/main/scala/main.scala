c590b9ba5ad89d2a8653984c4ee457b5ea731eb1
witt3rd <witt3rd@gmail.com>
2014-12-23 09:22:13 -0800

    baby step 1

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,8 +1,26 @@
 package linkedData
 
+import scala.collection.immutable
+
 object Main {
   def main(args: Array[String]) : Unit = {
-    val x = new NTriples("<foo> <bar> <baz> .").line.run()
-    println(x)
+
+    val ntDoc = 
+    """
+
+# This is a comment
+<http://www.w3.org/2004/02/skos/core#Concept> <http://www.w3.org/2000/01/rdf-schema#label> "Concept"@en .
+<http://www.w3.org/2004/02/skos/core#Concept> <http://www.w3.org/2000/01/rdf-schema#isDefinedBy> <http://www.w3.org/2004/02/skos/core> .
+<http://www.w3.org/2004/02/skos/core#Concept> <http://www.w3.org/2004/02/skos/core#definition> "An idea or notion; a unit of thought."@en .
+<http://www.w3.org/2004/02/skos/core#Concept> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#Class> .
+_:BX2Db3de8bfX3A149861d9206X3AX2D7ffe <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> <http://www.w3.org/2004/02/skos/core#Concept> .
+_:BX2Db3de8bfX3A149861d9206X3AX2D7ffe <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> _:BX2Db3de8bfX3A149861d9206X3AX2D7ffd .
+    """
+
+    val triples = ntDoc.lines map {line =>
+      new NTriples(line).line.run()
+    }
+
+    triples.foreach(println)
   }  
 }
--- src/main/scala/nTriples.scala
+++ src/main/scala/nTriples.scala
@@ -2,22 +2,91 @@ package linkedData
 
 import org.parboiled2._
 
-class NTriples(val input: ParserInput) extends Parser {
+class NTriples(val input: ParserInput) extends Parser with StringBuilding {
 
-  case class Triple(subj: String, pred: String, obj: String)
+  // Main parsed types
+  trait Line       // A parsed line; can either be Blank, Comment, or Triple
+  trait Subject    // A triple's subject; can either be a uriref or namednode
+  trait Predicate  // A triple's predicate; can either be a uriref or namednode
+  trait Object     // A triple's object; will always be a uriref
 
-  def line = rule {
-    capture(zeroOrMore(CharPredicate.Printable)) ~> 
-      (x => Triple(x,x,x))
+  case class UriRef(uri: String) extends Subject with Object with Predicate
+  case class NamedNode(name: String) extends Subject with Predicate
+
+  case class Blank() extends Line
+  case class Comment(text: String) extends Line
+  case class Triple(subj: Subject, pred: Predicate, obj: Object) extends Line
+
+  // ntripleDoc ::= line*  
+
+  // line ::= ws* (comment | triple) ? eoln  
+  def line = rule { blank | nonBlank }
+  private def blank = rule { zeroOrMore(ws) ~ EOI ~ push(Blank()) }
+  private def nonBlank = rule { (comment | triple) ~ EOI }
+
+  // comment ::= '#' (character - ( cr | lf ) )*  
+  private def comment = rule {
+    '#' ~ zeroOrMore(ws) ~ capture(zeroOrMore(CharPredicate.Printable)) ~> (Comment(_))
   }
 
-  def comment = ???
-  def triple = ???
-  def subject = ???
-  def predicate = ???
-  def obj = ???
-  def uriref = ???
-  def namedNode = ???
-  def literal = ???
-  def absoluteUri = ???
+  // triple ::= subject ws+ predicate ws+ object ws* '.' ws*   
+  private def triple = rule {
+    subject ~ oneOrMore(ws) ~ predicate ~ oneOrMore(ws) ~ obj ~ zeroOrMore(ws) ~ '.' ~ zeroOrMore(ws) ~>
+      ((s,p,o) => Triple(s,p,o))
+  }
+
+  // subject ::= uriref | namedNode   
+  private def subject: Rule1[Subject] = rule {
+    capture(zeroOrMore(CharPredicate.Printable)) ~> (UriRef(_))
+  }
+
+  // predicate ::= uriref   
+  private def predicate: Rule1[Predicate] = rule {
+    capture(zeroOrMore(CharPredicate.Printable)) ~> (UriRef(_))
+  }
+
+  // object ::= uriref | namedNode | literal   
+  private def obj: Rule1[Object] = rule {
+    capture(zeroOrMore(CharPredicate.Printable)) ~> (UriRef(_))
+  }
+
+  // uriref ::= '<' absoluteURI '>'  
+  private def uriref = ???
+
+  // namedNode ::= '_:' name  
+  private def namedNode = ???
+
+  // literal ::= '"' string '"'   
+  private def literal = ???
+
+  // ws ::= space | tab  
+  // def ws = rule { zeroOrMore(CharPredicate(" \t")) }
+  private def ws = rule { space | tab }  
+
+  // eoln ::= cr | lf | cr lf  
+  private def eoln = rule { cr | lf | cr ~ lf }  
+
+  // space ::= #x20 /* US-ASCII space - decimal 32 */   
+  private def space = CharPredicate(' ')
+
+  // cr ::= #xD /* US-ASCII carriage return - decimal 13 */  
+  private def cr = CharPredicate('\r')
+
+  // lf ::= #xA /* US-ASCII linefeed - decimal 10 */   
+  private def lf = CharPredicate('\n')
+
+  // tab ::= #x9 /* US-ASCII horizontal tab - decimal 9 */  
+  private def tab = CharPredicate('\t')
+
+  // string ::= character* with escapes. Defined in section Strings  
+  private def string = ???
+
+  // name ::= [A-Za-z][A-Za-z0-9]*   
+  private def name = ???
+
+  // absoluteURI ::= ( character - ( '<' | '>' | space ) )+   
+  private def absoluteUri = ???
+
+  // character ::= [#x20-#x7E] /* US-ASCII space to decimal 127 */ 
+  private def character = { CharPredicate.Printable }
 }
